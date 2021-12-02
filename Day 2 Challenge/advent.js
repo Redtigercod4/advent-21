9f94473c8b9a06df5071c74b60eb2254d505e643
Wes <76014409+Redtigercod4@users.noreply.github.com>
2021-12-02 17:05:36 +0000

    Day 2

--- Day 2 Challenge/advent.js
+++ Day 2 Challenge/advent.js
@@ -1,4 +1,27 @@
 const fs = require('fs')
 
-const data = fs.readFileSync('data.txt','utf8')
-console.log(data)
+    const data = fs.readFileSync('data.txt','utf8').toString().split('\n');
+     console.log(data)
+
+     let forward = 0, depth = 0, aim = 0, aimedDepth = 0;
+for(let i = 0; i <= data.length; i++) {
+    const command = data[i]?.split(' ');
+    let direction = command?.[0];
+    let amount = parseInt(command?.[1]);
+    switch(direction) {
+        case "forward":
+            forward += amount;
+            if(aim) aimedDepth += amount * aim;
+            break;
+        case "up":
+            depth -= amount;
+            aim -= amount;
+            break;
+        case "down":
+            depth += amount;
+            aim += amount;
+            break;
+    }
+}
+console.log("forward x depth:", forward * depth);
+console.log("forward x aimedDepth:", forward * aimedDepth);
